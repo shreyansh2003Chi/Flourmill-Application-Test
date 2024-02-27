f2ab21203d8977a19a0b671300e0d00c490b803e
SHREYANSH <shreyanshchichi@gmail.com>
2024-02-27 07:24:51 +0530

    new changes

--- lib/UIComponents/CustomButton.dart
+++ lib/UIComponents/CustomButton.dart
@@ -1,15 +1,60 @@
+// import 'package:flutter/material.dart';
+//
+// class CustomButton extends StatelessWidget {
+//   final String label;
+//   final IconData icon;
+//   final VoidCallback onPressed;
+//
+//   const CustomButton({
+//     Key? key,
+//     required this.label,
+//     required this.icon,
+//     required this.onPressed,
+//   }) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       height: 60,
+//       width: 400,
+//       child: ElevatedButton(
+//         onPressed: onPressed,
+//         style: ElevatedButton.styleFrom(
+//           textStyle: const TextStyle(
+//             fontSize: 20,
+//             fontWeight: FontWeight.bold,
+//           ),
+//           backgroundColor: Colors.white70,
+//           shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.circular(15),
+//           ),
+//         ),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(label),
+//             const SizedBox(width: 30),
+//             Icon(icon),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
   final String label;
   final IconData icon;
   final VoidCallback onPressed;
+  final Color buttonColor; // New property to specify button color
 
   const CustomButton({
     Key? key,
     required this.label,
     required this.icon,
     required this.onPressed,
+    this.buttonColor = const Color(0xFFB9CCF8), // Default button color
   }) : super(key: key);
 
   @override
@@ -24,7 +69,7 @@ class CustomButton extends StatelessWidget {
             fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
-          backgroundColor: Colors.white70,
+          backgroundColor: buttonColor, // Use the specified button color
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15),
           ),
--- lib/pages/OTPVerification.dart
+++ lib/pages/OTPVerification.dart
@@ -1,96 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-import '../UIComponents/CustomButton.dart';
-
-void main() {
-  runApp(const OTPVerification());
-}
+import 'package:pinput/pinput.dart';
 
 class OTPVerification extends StatelessWidget {
-  const OTPVerification({super.key});
+  const OTPVerification({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Verify Your OTP Here'),
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(30.0, 250.0, 30.0, 0.0),
-          child: Form(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CustomOTPTextField(),
-                CustomOTPTextField(),
-                CustomOTPTextField(),
-                CustomOTPTextField(),
-              ],
-            ),
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 60,
+      textStyle: const TextStyle(
+        fontSize: 22,
+        color: Colors.black,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.green.shade100,
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: Colors.transparent),
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        title: const Text('OTP TextField'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Container(
+          margin: const EdgeInsets.only(top: 30),
+          width: double.infinity,
+          child: Column(
+            children: [
+              const SizedBox(
+                  height: 150,
+                  width: 150,
+                  child: Image(
+                    image: AssetImage("assets/otp.png"),
+                  )),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 40),
+                child: const Text(
+                  "Verification",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 0),
+                child: const Text(
+                  "Enter the code sent to your number",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 40),
+                child: const Text(
+                  "+91 74479 58640",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Pinput(
+                length: 5,
+                defaultPinTheme: defaultPinTheme,
+                keyboardType: TextInputType.number,
+                focusedPinTheme: defaultPinTheme.copyWith(
+                  decoration: defaultPinTheme.decoration!.copyWith(
+                    border: Border.all(color: Colors.green),
+                  ),
+                ),
+                onCompleted: (pin) => debugPrint(pin),
+              ),
+            ],
           ),
         ),
       ),
     );
-  }
-}
-
-class CustomOTPTextField extends StatefulWidget {
-  @override
-  _CustomOTPTextFieldState createState() => _CustomOTPTextFieldState();
-}
-
-class _CustomOTPTextFieldState extends State<CustomOTPTextField> {
-  final FocusNode _focusNode = FocusNode();
-  TextEditingController _controller = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener(() {
-      if (_controller.text.isEmpty) {
-        _focusNode.previousFocus();
-      } else if (_controller.text.length == 1) {
-        _focusNode.nextFocus();
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 68,
-      height: 64,
-      child: TextFormField(
-        controller: _controller,
-        focusNode: _focusNode,
-        textAlign: TextAlign.center,
-        style: Theme.of(context).textTheme.headline6,
-        keyboardType: TextInputType.number,
-        inputFormatters: [
-          LengthLimitingTextInputFormatter(1),
-          FilteringTextInputFormatter.digitsOnly
-        ],
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    _focusNode.dispose();
-    super.dispose();
   }
 }
